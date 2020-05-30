e87dbfc08cc5203570e270e130ce9a26060ec789
rigo0523 <limon1285@gmail.com>
2020-05-29 21:37:38 -0500

    got axios working

--- starwars/src/components/Character.jsx
+++ starwars/src/components/Character.jsx
@@ -6,20 +6,33 @@ import CharacterCard from "./CharacterCard"
 
 
 
+
+
 const Character = () => {
     const [character, setCharacter] = useState([]);
 
+    useEffect(() => {
     axios
         .get("https://rickandmortyapi.com/api/character/?page=2")
+
         .then(response => {
-            setCharacter(response.data)
-            console.log(response, 'response for character data')
+            console.log(response.data.results, 'data results')
+            setCharacter(response.data.results)
         })
+
+        .catch(error => {
+            console.log(error, "there's an error here")
+        })
+    },[])
 
     return (
         <div className="container">
-
+            {character.map(data => { 
+                return <CharacterCard data={data} />
+            })}
 
         </div>
     )
 }
+
+export default Character;
--- starwars/src/components/CharacterCard.jsx
+++ starwars/src/components/CharacterCard.jsx
@@ -1,11 +1,27 @@
 import React from "react"
 import styled from "styled-components"
 
+const Card = styled.div`
+    background: gray;
+    width: 23%;
+`;
+ 
+const Title = styled.h1`
+    text-align: center;
+    background: gray;
+`;
+
+const CharacterCard = ({data}) => {
 
 
-const CharacterCard = () => {
-
-   return <h1>hello</h1>
+    return (
+        <Card>
+            <Title>Character Name:  </Title>
+            
+            <img src={data.image} alt="rick and morty characters"/>
+        </Card>
+    )
+   
 }
 
 export default CharacterCard;
